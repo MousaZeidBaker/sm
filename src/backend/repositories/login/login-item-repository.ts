373d21f8aa87143fd40a73fab74e6980975ea5f1
Mousa Zeid Baker <mzeb@moodagent.com>
2021-05-09 16:35:53 +0200

    Add login item repository

---
+++ src/backend/repositories/login/login-item-repository.ts
@@ -0,0 +1,54 @@
+import { LoginItem } from '../../models/login/login-item'
+import SSM from 'aws-sdk/clients/ssm'
+import ResourceGroupsTaggingAPIClient from 'aws-sdk/clients/resourcegroupstaggingapi'
+
+export declare class LoginItemRepository {
+  constructor(ssmClient: SSM, resourceGroupsTaggingAPIClient: ResourceGroupsTaggingAPIClient)
+
+  /**
+   * Create item
+   * 
+   * @param {string} title
+   * @param {string} path
+   * @param {string} username
+   * @param {string} secret
+   * @return {Promise<LoginItem>}
+   */
+  create(title?: string, path?: string, username?: string, secret?: string): Promise<LoginItem>
+
+  /**
+   * Get item
+   * 
+   * @param {string} id
+   * @param {number} version
+   * @return {Promise<LoginItem>}
+   */
+  get(id: string, version?: number): Promise<LoginItem>
+
+  /**
+   * List items
+   * 
+   * @return {Promise<LoginItem[]>}
+   */
+  list(): Promise<LoginItem[]>
+
+  /**
+   * Update item
+   * 
+   * @param {string} id
+   * @param {string} title
+   * @param {string} path
+   * @param {string} username
+   * @param {string} secret
+   * @return {Promise<LoginItem>}
+   */
+  update(id: string, title?: string, path?: string, username?: string, secret?: string): Promise<LoginItem>
+
+  /**
+   * Delete item
+   * 
+   * @param {string} id
+   * @return {Promise<void>}
+   */
+  delete(id: string): Promise<void>
+}
